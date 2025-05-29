c7e67aa2505c93c812eb344694ba64932b783352
Ganesh Saravanan <57284555+sourceganesh@users.noreply.github.com>
2025-05-29 17:40:27 +0530

    Apply themed colors and fix image links

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
+const path = require('path');
+
 const source = fs.readFileSync(require.resolve('./App.js'), 'utf8');
-if (/YBM Techno Craft/i.test(source)) {
-  console.log('Test passed');
-} else {
+if (!/YBM Techno Craft/i.test(source)) {
   console.error('Header text not found');
   process.exit(1);
 }
+
+const imgRegex = /["'`](images\/[^"'`]+)["'`]/g;
+let match;
+const missing = [];
+while ((match = imgRegex.exec(source))) {
+  const imgPath = path.resolve(__dirname, '..', match[1]);
+  if (!fs.existsSync(imgPath)) {
+    missing.push(match[1]);
+  }
+}
+
+if (missing.length) {
+  console.error('Missing images:', missing.join(', '));
+  process.exit(1);
+}
+
+console.log('Test passed');
